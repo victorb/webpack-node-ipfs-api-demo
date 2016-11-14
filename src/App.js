bc6232bb1bc2ec7fd0b9b08471ccb99d8cd9d5fc
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-11-14 12:18:40 +0100

    Pass standard lint

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import ipfsAPI from 'ipfs-api'
 
 var ipfs = ipfsAPI('localhost', '5001')
-const string_to_use = "Hello World From Webpack"
+const stringToUse = 'Hello World From Webpack'
 
 export default class App extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			id: null,
-			version: null,
-			protocol_version: null,
-			added_file_hash: null,
-			added_file_contents: null
-		}
-	}
-	componentDidMount() {
-		ipfs.id((err, res) => {
-			if(err) {
-				console.error(err)
-				return
-			}
-			this.setState({
-				id: res.id,
-				version: res.agentVersion,
-				protocol_version: res.protocolVersion
-			})
-		})
-		ipfs.add([new Buffer(string_to_use)], (err, res) => {
+  constructor (props) {
+    super(props)
+    this.state = {
+      id: null,
+      version: null,
+      protocol_version: null,
+      added_file_hash: null,
+      added_file_contents: null
+    }
+  }
+  componentDidMount () {
+    ipfs.id((err, res) => {
+      if (err) throw err
+      this.setState({
+        id: res.id,
+        version: res.agentVersion,
+        protocol_version: res.protocolVersion
+      })
+    })
+    ipfs.add([new Buffer(stringToUse)], (err, res) => {
+      if (err) throw err
       const hash = res[0].hash
-			this.setState({added_file_hash: hash})
-			ipfs.cat(hash, (err, res) => {
+      this.setState({added_file_hash: hash})
+      ipfs.cat(hash, (err, res) => {
+        if (err) throw err
         let data = ''
         res.on('data', (d) => {
           data = data + d
@@ -39,31 +38,30 @@ export default class App extends Component {
         res.on('end', () => {
           this.setState({added_file_contents: data})
         })
-			})
-			ipfs.dht.findprovs(hash, (err, res) => {
-				console.log('found provs!')
-				console.log(err, res)
-			})
-		})
-	}
-  render() {
-    return (
-			<div style={{textAlign: 'center'}}>
-				<h1>Everything is working!</h1>
-				<p>Your ID is <strong>{this.state.id}</strong></p>
-				<p>Your IPFS version is <strong>{this.state.version}</strong></p>
-				<p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
-				<div>
-					<div>
-						Added a file!<br/>
-						{this.state.added_file_hash}
-					</div>
-					<div>
-						Contents of this file:<br/>
-						{this.state.added_file_contents}
-					</div>
-				</div>
-			</div>
-    );
+      })
+      ipfs.dht.findprovs(hash, (err, res) => {
+        if (err) throw err
+        console.log('found provs!')
+        console.log(err, res)
+      })
+    })
+  }
+  render () {
+    return <div style={{textAlign: 'center'}}>
+      <h1>Everything is working!</h1>
+      <p>Your ID is <strong>{this.state.id}</strong></p>
+      <p>Your IPFS version is <strong>{this.state.version}</strong></p>
+      <p>Your IPFS protocol version is <strong>{this.state.protocol_version}</strong></p>
+      <div>
+        <div>
+          Added a file! <br />
+          {this.state.added_file_hash}
+        </div>
+        <div>
+          Contents of this file: <br />
+          {this.state.added_file_contents}
+        </div>
+      </div>
+    </div>
   }
 }
